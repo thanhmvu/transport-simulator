a408fb8ffdecb25dc058eb0c69c177f68bc593be
githmith <vuminhthanh2305@gmail.com>
2017-03-27 13:36:46 -0400

    Add comment for SenderTransport

--- src/transport/SenderTransport.java
+++ src/transport/SenderTransport.java
@@ -15,21 +15,46 @@ public class SenderTransport
     public SenderTransport(NetworkLayer nl){
         this.nl=nl;
         initialize();
-
     }
 
+    /**
+     * This routine will be called once, 
+     * before any of other sender routines are called. 
+     * It can be used to do any required initialization. 
+     */
     public void initialize()
     {
     }
-
+    
+    /**
+     * This routine will be called whenever 
+     * the upper layer at the sending side has a message to send. 
+     * The data in such a message should be delivered 
+     * in-order and correctly to the receiving side upper layer.
+     * 
+     * msg - message contains data to be sent to the other side (B-side)
+     */
     public void sendMessage(Message msg)
     {
     }
-
+    
+    /**
+     * This routine will be called whenever 
+     * a packet sent from the receiver arrives at the sender. 
+     * Packet is sent from the receiver (B-side) and is possibly corrupted.
+     * 
+     * pkt - the receiving packet
+     */
     public void receiveMessage(Packet pkt)
     {
     }
-
+    
+    /**
+     * This routine will be called when the sender's timer expires,
+     * thus generating a timer interrupt. 
+     * This routine should be used to control the retransmission of packets. 
+     * See starttimer() and stoptimer() for how the timer is started and stopped.
+     */
     public void timerExpired()
     { 
     }
