20d08b39de01c0481d7d464c2d3f5a211c44ec06
hongha912 <hongha912@gmail.com>
2017-03-30 00:54:04 -0400

    Some cosmetic changes

--- src/transport/NetworkLayer.java
+++ src/transport/NetworkLayer.java
@@ -1,49 +1,56 @@
 package transport;
 
 import java.util.*;
+
 /**
  * A class which represents the transoprt layer for both sender and receiver.
  */
 
-public class NetworkLayer
-{
+public class NetworkLayer {
+
     float lossProbability; //probablity of losing a packet
-    float currProbability; //probability of corruping a packet
-    Timeline tl; 
+    float corrProbability; //probability of corruping a packet
+    Timeline tl;
     Random ran; //random number generator for losing packets.
 
     /**
      * Create a network layer
+     *
      * @param lp The loss probability
      * @param cp The corrupting probability
      * @param tl The timeline to send events to
      */
-    public NetworkLayer(float lp, float cp,Timeline tl)
-    {
-        lossProbability=lp;
-        currProbability=cp;
-        this.tl=tl;
+    public NetworkLayer(float lp, float cp, Timeline tl) {
+        lossProbability = lp;
+        corrProbability = cp;
+        this.tl = tl;
         ran = new Random();
     }
 
-    //sending packet if it is not lost, and corruping it if necessary.
-    public void sendPacket(Packet pkt, int to)
-    {
-        if(ran.nextDouble()<lossProbability)
-        {
-            if(NetworkSimulator.DEBUG>1)
+    /**
+     * Sending packet if it is not lost, and corrupting it if necessary.
+     *
+     * @param pkt The packet to be sent
+     * @param to Who the packet is being sent to (Event.SENDER or
+     * Event.RECEIVER)
+     */
+    public void sendPacket(Packet pkt, int to) {
+        if (ran.nextDouble() < lossProbability) {
+            if (NetworkSimulator.DEBUG > 1) {
                 System.out.println("Packet seq:" + pkt.getSeqnum() + " ack: " + pkt.getAcknum() + " lost");
-            return;    
+            }
+            return;
         }
-        if(ran.nextDouble()<currProbability)
-        {
-            if(NetworkSimulator.DEBUG>1)
+        if (ran.nextDouble() < corrProbability) {
+            if (NetworkSimulator.DEBUG > 1) {
                 System.out.println("Packet seq:" + pkt.getSeqnum() + " ack: " + pkt.getAcknum() + " corruped");
+            }
             pkt.corrupt();
         }
-        if(NetworkSimulator.DEBUG>1)
+        if (NetworkSimulator.DEBUG > 1) {
             System.out.println("Packet seq:" + pkt.getSeqnum() + " ack: " + pkt.getAcknum() + " sent");
-        tl.createArriveEvent(pkt,to);
+        }
+        tl.createArriveEvent(pkt, to);
     }
 
 }
--- src/transport/ReceiverTransport.java
+++ src/transport/ReceiverTransport.java
@@ -19,12 +19,26 @@ public class ReceiverTransport {
      * This routine will be called once, before any of your other receiver
      * routines are called. It can be used to do any required initialization.
      */
-    public void initialize() {
+    public final void initialize() {
         if (!usingTCP) {
-            
+            initializeGBN();
         } else {
-            
+            initializeTCP();
         }
+    }
+    
+    /**
+     * Initialize Go-Back-N routine
+     */
+    private void initializeGBN() {
+        
+    }
+    
+    /**
+     * Initialize TCP routine
+     */
+    private void initializeTCP() {
+        
     }
 
     /**
@@ -35,10 +49,26 @@ public class ReceiverTransport {
      */
     public void receiveMessage(Packet pkt) {
         if (!usingTCP) {
-            
+            receiveMessageGBN(pkt);
         } else {
-            
+            receiveMessageTCP(pkt);
         }
+    }
+    
+    /**
+     * Implement receive message for Go-Back-N routine
+     * @param pkt the (possibly corrupted) packet sent from the sender
+     */
+    private void receiveMessageGBN(Packet pkt) {
+        
+    }
+    
+    /**
+     * Implement receive message for TCP routine
+     * @param pkt the (possibly corrupted) packet sent from the sender
+     */
+    private void receiveMessageTCP(Packet pkt) {
+        
     }
 
     public void setProtocol(int n) {
